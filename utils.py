fb54f0d843ff894c13b0214453cdbb0994e6e81c
seblful <alesha.chimba@gmail.com>
2024-01-30 21:29:26 +0300

    [EDIT] visualize funcs

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
-import math
 import os
 import json
+
+import math
+
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 
 
 def create_yolo_bboxes(json_min,
@@ -76,6 +80,48 @@ class_mapping = {"number": 0,
                  "table": 3,
                  "appendix": 4}
 
-create_yolo_bboxes(json_min=json_file_path,
-                   output_dir=output_dir,
-                   class_mapping=class_mapping)
+
+def read_yolo_obb(obb_label_path):
+    with open(obb_label_path, 'r') as f:
+        lines = f.readlines()
+        labels = [list(map(float, line.strip().split())) for line in lines]
+    return labels
+
+
+def visualize_labels(raw_data_dir):
+    # Define filling colors
+    colors = ["blue", "green", "yellow", "red", "black"]
+
+    # Define images, and labels dirs
+    images_dir = os.path.join(raw_data_dir, 'images')
+    labels_dir = os.path.join(raw_data_dir, "labels")
+
+    # Iterating through images and labels
+    for image_name, label_name in zip(os.listdir(images_dir), os.listdir(labels_dir)):
+        # Read image
+        full_image_path = os.path.join(images_dir, image_name)
+        image = Image.open(full_image_path)
+        image_width, image_height = image.size[0], image.size[1]
+        draw = ImageDraw.Draw(image)
+
+        # Read label
+        full_label_path = os.path.join(labels_dir, label_name)
+        labels = read_yolo_obb(full_label_path)
+
+        # Iterating through each label, reformat label and draw lines
+        for label in labels:
+            class_index, x1, y1, x2, y2, x3, y3, x4, y4 = label
+            x1, x2, x3, x4 = [x * image_width for x in (x1, x2, x3, x4)]
+            y1, y2, y3, y4 = [y * image_height for y in (y1, y2, y3, y4)]
+            points = [(x1, y1), (x2, y2), (x3, y3), (x4, y4), (x1, y1)]
+            draw.line(points, fill=colors[int(class_index)], width=10)
+
+        plt.imshow(image)
+        plt.show()
+
+
+# visualize_labels(raw_data_dir="key-detection/raw-data")
+
+# create_yolo_bboxes(json_min=json_file_path,
+#                    output_dir=output_dir,
+#                    class_mapping=class_mapping)
